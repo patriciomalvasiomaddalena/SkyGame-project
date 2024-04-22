5586267c9fbec1c41dadcc0c285672c9d5e7ab65
patriciomalvasiomaddalena <130863754+patriciomalvasiomaddalena@users.noreply.github.com>
2024-04-21 21:47:11 -0300

    RepoTest
    con suerte ultima vez que creo un repo

---
+++ Assets/RepoTest.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepoTest : MonoBehaviour
+{
+    public void Start()
+    {
+        Debug.Log("Haro every - nyan!");
+    }
+
+
+}
