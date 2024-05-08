cc8a51ca626d8ac8f9b2d76529e4b5c2a090bb5b
Ninf0 <118858421+Ninf0@users.noreply.github.com>
2024-05-07 22:57:13 -0300

    Menu
    base menu

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Menu : MonoBehaviour
+{
+
+    AudioSource AU;
+
+
+    public void Jugar()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+    }
+
+    public void Salir()
+    {
+        Application.Quit();
+    }
+}
